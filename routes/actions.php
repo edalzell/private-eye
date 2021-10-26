c401428d8066a4d2f1eaface8a133e8b86ce2cff
edalzell <erin@silentz.co>
2021-10-26 15:10:30 -0700

    gotta be logged in to do any of this

--- routes/actions.php
+++ routes/actions.php
@@ -4,7 +4,7 @@ use Edalzell\PrivateEye\DownloadController;
 use Edalzell\PrivateEye\ShowController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('private-eye.')->group(function () {
+Route::name('private-eye.')->middleware('auth')->group(function () {
     Route::get('download/{code}', [DownloadController::class, '__invoke'])->name('download');
     Route::get('show/{code}', [ShowController::class, '__invoke'])->name('show');
 });
--- src/DownloadController.php
+++ src/DownloadController.php
@@ -2,16 +2,10 @@
 
 namespace Edalzell\PrivateEye;
 
-use Statamic\Facades\User;
-
 class DownloadController extends BaseController
 {
     public function __invoke(string $code)
     {
-        if (! User::current()) {
-            return back()->withErrors('Gotta be logged in', 'download');
-        }
-
         if (! file_exists($path = $this->asset($code)->resolvedPath())) {
             return back()->withErrors('File not found', 'download');
         }
